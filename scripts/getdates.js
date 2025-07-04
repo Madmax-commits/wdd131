61e8e8ffd11daddd92996e3fc124750b885a0590
Madmax-commits <tobamicar@gmail.com>
2025-07-04 05:33:32 +0100

    html

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Insert current year in footer
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Insert last modified date in footer
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
